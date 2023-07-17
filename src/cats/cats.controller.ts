c3d949a095c3a7216fcf1a3565e4e5bd945d1d56
jinhan38 <jinhan38@naver.com>
2023-07-18 00:03:49 +0900

    예외처리 학습중

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Delete, Get, HttpCode, Patch, Post, Put} from '@nestjs/common';
+import {Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Patch, Post, Put} from '@nestjs/common';
 import {CatsService} from "./cats.service";
 
 @Controller('cats')
@@ -6,10 +6,18 @@ export class CatsController {
     constructor(private readonly catsService: CatsService) {
     }
 
-
     @Get()
     @HttpCode(200) // 크롬에서 동적 메소드 실행시켰을 때 status 304로 나온다. 캐시를 저장하기 때문이다.
     getAllCat() {
+        // 일반적인 에러 처리
+        // throw new HttpException('api is broken', 401);
+
+        // 커스텀 에러 처리
+        throw new HttpException({
+            status: HttpStatus.FORBIDDEN,
+            error: 'This is a custom message',
+            success: false,
+        }, HttpStatus.FORBIDDEN);
         return 'get all cats';
     }
 
